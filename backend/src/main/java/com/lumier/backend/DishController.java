7f3b70082e82cebafac3a2fc7398f7a6fb00d4cc
ТвоёИмя <@email.com>
2026-05-13 19:02:23 +0500

    feat: add Swagger UI and implement CRUD for dishes

--- backend/src/main/java/com/lumier/backend/DishController.java
+++ backend/src/main/java/com/lumier/backend/DishController.java
@@ -5,15 +5,28 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/dishes") // Теперь адрес будет: /api/dishes
+@RequestMapping("/api/dishes")
 @CrossOrigin(origins = "*")
-@RequiredArgsConstructor // Это создаст конструктор для dishRepository
+@RequiredArgsConstructor
 public class DishController {
 
     private final DishRepository dishRepository;
 
+    // Получить все блюда
     @GetMapping
     public List<Dish> getAllDishes() {
         return dishRepository.findAll();
+    }
+
+    // отправьте данные блюда, и они сохранятся в базу
+    @PostMapping
+    public Dish createDish(@RequestBody Dish dish) {
+        return dishRepository.save(dish);
+    }
+
+    //удаление по айди
+    @DeleteMapping("/{id}")
+    public void deleteDish(@PathVariable Long id) {
+        dishRepository.deleteById(id);
     }
 }
